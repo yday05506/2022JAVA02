8147711e64c5812cafe3a4e0335a2a825339677c
yday <yangdayeon2@gmail.com>
2022-04-19 15:51:14 +0900

    소리 올리고 내리기

--- src/TextInheritance11/SoundExam.java
+++ src/TextInheritance11/SoundExam.java
@@ -4,7 +4,7 @@ public class SoundExam {
     public static void main(String[] args) {
         Sound radio = new Radio();  // UpCasting과 유사
         // radio 객체 변수는 Radio 클래스에서 재정의된 메소드에 접근
-        Sound tv = new TV();    // UpCasting과 유사
+        Sound tv = new TV();    // UpCasting과 유사함
 
         radio.SoundUp(5);
         tv.SoundUp(5);
