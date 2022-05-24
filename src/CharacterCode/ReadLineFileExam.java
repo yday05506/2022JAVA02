d6bad2f5cc37b793fd1602909707d66902c4e792
yday <yangdayeon2@gmail.com>
2022-05-24 15:44:39 +0900

    파일로부터 한 줄 단위 문자 읽기(주석 추가)

--- src/CharacterCode/ReadLineFileExam.java
+++ src/CharacterCode/ReadLineFileExam.java
@@ -6,7 +6,11 @@ public class ReadLineFileExam {
     public static void main(String[] args) {
         String fname = "C:/Temp/BufferInput.txt";
         try {
+            // fr은 "BufferedInput.txt" 파일을 가리키는 객체 변수
             FileReader fr = new FileReader(fname);
+            // br은 "BufferedInpute.txt" 파일을 가리키는 객체 변수이고
+            // 버퍼 클래스에 있는 readLine() 메소드를 사용하기 위함
+            // 버퍼 클래스는 입력과 출력의 속도를 증가 시켜줌
             BufferedReader br = new BufferedReader(fr);
             String s = "";
 
