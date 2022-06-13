1d8cba8cc87f1797e8cec1ab2adc48ffdfe71fa5
yday <yangdayeon2@gmail.com>
2022-06-13 23:06:27 +0900

    성적 처리 프로그램(미완성)

--- src/Practice/WritingD.java
+++ src/Practice/WritingD.java
@@ -1,13 +1,13 @@
-package Practice;;
+package Practice;
 
-import java.io.IOException;
+import java.io.*;
 
 public class WritingD {
     public static void main(String[] args) {
         ReadingD wd = new ReadingD();
         try {
-            wd.readingData ("C:/Temp/sung_read.txt");
-            wd.writingData("C:/Temp/sung_write.txt");
+            wd.ReadingD("C:/Temp/sung_read.txt");
+            wd.WritingD("C:/Temp/sung_write.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
