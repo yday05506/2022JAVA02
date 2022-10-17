d6d235c8d4c1d982ecd76e76f066ad26b0eb71b5
yday <yangdayeon2@gmail.com>
2022-10-17 21:23:27 +0900

    Revert "성적 처리 프로그램(미완성)"
    This reverts commit 1d8cba8cc87f1797e8cec1ab2adc48ffdfe71fa5.

--- src/Practice/ReadingD.java
+++ src/Practice/ReadingD.java
@@ -5,56 +5,24 @@ import java.util.*;
 
 public class ReadingD {
     int length;
-    String[] s_num = new String[10];
-    String[] name = new String[10];;
+    String[] s_number = new String[10];
+    String[] name = new String[10];
     double[] korean = new double[10];
-    double[] math = new double[10];
     double[] english = new double[10];
+    double[] math = new double[10];
     double[] total = new double[10];
     double[] avg = new double[10];
-    String[] grade = new String[10];
-    int[] rank = new int[10];
-    int ranker;
+    String[] score = new String[10];
+    double[] grade = new double[10];
 
-    public void WritingD(String fname) throws IOException {
-        // String fname = "C:/Temp/writing.txt"
+    public void readingData(String fname) throws IOException {
         FileWriter fw = null;
         BufferedWriter bw = null;
         try {
-            fw = new FileWriter(new File(fname));
-            // fw는 "writing.txt"파일을 가리키는 객체변수이다.
-            bw = new BufferedWriter(fw);
-            // bw는 fw가 라리키는 파일을 가리키며, 버퍼라는 클래스를 추가하여 사용한다.
-
-            bw.write("=============================================================");
-            bw.newLine();
-            bw.write("학번" + "\t\t" + "이름" + "\t\t" + "국어" + "\t\t" + "영어" + "\t\t" + "수학" + "\t\t" + "총점" + "\t\t" + "평균" + "\t\t" + "학점" + "\t\t" + "등수");
-            bw.newLine();
-            bw.write("=============================================================");
-            bw.newLine();
-
-            for (int j = 0; j < length; j++) {
-                bw.write(s_num[j] + "\t" + name[j] + "\t" + String.format("%.0f", korean[j]) + "\t\t" + String.format("%.0f", math[j]) + "\t\t" + String.format("%.0f", english[j])
-                        + "\t\t" + total[j] + "\t" + String.format("%.1f", avg[j]) + "\t\t" + grade[j]);
-                // bw가 가리키는 파일 "writing.txt"에 출력한다.
-                bw.newLine(); // BufferedWriter클래스는 newLine()메소드를 가지고 있다.
-                // newLine()는 줄바꿈
-            }
-            bw.close(); // 파일을 닫을때는 객체생성의 역순으로 한다.
-            fw.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("잘못된 파일 이름을 입력했습니다");
-        }
-    }
-
-    public void ReadingD(String fname) throws IOException {
-        // String fname = "C:/Temp/writed.txt"
-        // 배열에 파일으로 부터 읽어들인 데이터를 저장한다.
-        try {
-            String csvStr = "";
-            String tmpStr = "";
             FileReader fr = new FileReader(new File(fname));
             BufferedReader br = new BufferedReader(fr);
+            String csvStr = "";
+            String tmpStr = "";
             do {
                 tmpStr = br.readLine();
                 if (tmpStr != null) {
@@ -62,26 +30,40 @@ public class ReadingD {
                 }
             } while (tmpStr != null);
             StringTokenizer parse = new StringTokenizer(csvStr, "\t");
-            length = parse.countTokens() / 5;
+            int length = parse.countTokens() / 5;
+            String[] s_number = new String[length];
+            String[] name = new String[length];
+            double[] korean = new double[length];
+            double[] english = new double[length];
+            double[] math = new double[length];
+            double[] total = new double[length];
+            double[] avg = new double[length];
+            String[] score = new String[length];
+            double[] grade = new double[length];
 
             for (int i = 0; i < length; i++) {
-                s_num[i] = parse.nextToken();
+                s_number[i] = parse.nextToken();
                 name[i] = parse.nextToken();
-                korean[i] = Double.valueOf(parse.nextToken()).doubleValue();
+                score[i] = parse.nextToken();
                 math[i] = Double.valueOf(parse.nextToken()).doubleValue();
                 english[i] = Double.valueOf(parse.nextToken()).doubleValue();
+                korean[i] = Double.valueOf(parse.nextToken()).doubleValue();
+                grade[i] = i;
                 total[i] = math[i] + english[i] + korean[i];
                 avg[i] = total[i] / 3.0;
-                if (avg[i] >= 90 && avg[i] <= 100) grade[i] = "A";
-                else if(avg[i] >= 80) grade[i] = "B";
-                else if(avg[i] >= 70) grade[i] = "C";
-                else if(avg[i] >= 60) grade[i] = "D";
-                else grade[i] = "F";
-                rank[i] = Arrays.sort((int)avg);
             }
 
-        } catch (FileNotFoundException e) {
-            System.out.println("잘못된 파일 이름을 입력했습니다");
+            System.out.println("학번" + "\t" + "이름" + "\t" + "국어" + "\t" + "영어" + "\t" + "수학" + "\t" + "총점" + "\t" + "평균" + "\t" + "학점" + "\t" + "등수");
+            System.out.println("=========================================================================================");
+            for (int j = 0; j < length; j++) {
+                System.out.println(s_number[j] + "\t" + name[j] + "\t" + (int)(korean[j]) + "\t" + (int)(english[j]) + "\t" + (int)(math[j]) + "\t" + (int)(total[j]) + "\t" + (int)(avg[j]) + "\t" + score[j] + "\t" + (int)(grade[j]));
+            }
+        } catch (
+                FileNotFoundException e) {
+            System.out.println("잘못된 파일 이름을 입력했습니다.");
         }
+    }
+
+    public void writingData(String s) {
     }
 }
--- src/Practice/WritingD.java
+++ src/Practice/WritingD.java
@@ -1,13 +1,13 @@
-package Practice;
+package Practice;;
 
-import java.io.*;
+import java.io.IOException;
 
 public class WritingD {
     public static void main(String[] args) {
         ReadingD wd = new ReadingD();
         try {
-            wd.ReadingD("C:/Temp/sung_read.txt");
-            wd.WritingD("C:/Temp/sung_write.txt");
+            wd.readingData ("C:/Temp/sung_read.txt");
+            wd.writingData("C:/Temp/sung_write.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
