d9a7e75060f445eaed3ee49166795bcc08972795
yday <yangdayeon2@gmail.com>
2022-10-17 21:22:05 +0900

    Revert "BalanceInsufficientException"
    This reverts commit 3d32c118b1c3cd1b3e20e539934afe95976ad283.

--- src/exception02/BalanceInsufficientException.java
+++
@@ -1,9 +0,0 @@
-package exception02;
-
-// 사용자 예외를 정의할 때 exception 클래스를 상속
-public class BalanceInsufficientException extends Exception {
-    public BalanceInsufficientException() {}
-    public BalanceInsufficientException(String message) {
-        super(message);
-    }
-}
